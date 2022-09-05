c5a58263da00cd91dea65b343de02b5773bde046
Joaquín Sánchez Jiménez <joaquin.sanchez@fi2net.es>
2022-09-05 11:00:51 +0200

    chore: add default layout

---
+++ src/layouts/default.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+//
+</script>
+
+<template>
+  <v-app>
+    <v-main>
+      <v-container
+        class="fill-width fill-height align-start justify-start py-0"
+        fluid
+      >
+        <v-row>
+          <v-col>
+            <router-view :key="$route.fullPath" />
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
